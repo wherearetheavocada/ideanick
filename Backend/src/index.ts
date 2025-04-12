34b1ba01a933bdedab1d1e060f7a9bca988c4bc1
Nikita <6347274@mail.ru>
2025-04-12 18:52:13 +0300

    Creating an endpoint that returns JSON

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,8 +1,38 @@
 import express from 'express'
 
+const ideas = [
+  {
+    nick: 'cool-idea-nick-1',
+    name: 'Idea 1',
+    description: 'Description of idea 1...',
+  },
+  {
+    nick: 'cool-idea-nick-2',
+    name: 'Idea 2',
+    description: 'Description of idea 2...',
+  },
+  {
+    nick: 'cool-idea-nick-3',
+    name: 'Idea 3',
+    description: 'Description of idea 3...',
+  },
+  {
+    nick: 'cool-idea-nick-4',
+    name: 'Idea 4',
+    description: 'Description of idea 4...',
+  },
+  {
+    nick: 'cool-idea-nick-5',
+    name: 'Idea 5',
+    description: 'Description of idea 5...',
+  },
+]
 const expressApp = express()
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
+})
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas)
 })
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
