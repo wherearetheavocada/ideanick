d5bf7b745fcf2357ac577e30c3a098c7da41f11f
Nikita <6347274@mail.ru>
2025-04-12 18:21:13 +0300

    install express

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello!')
+import express from 'express'
+
+const expressApp = express()
+expressApp.get('/ping', (req, res) => {
+  res.send('pong')
+})
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
+})
